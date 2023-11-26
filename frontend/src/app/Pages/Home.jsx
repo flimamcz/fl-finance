139ef325fed66599e45971dfc951b057968af1d0
Filipe Lima <cimofelipe4@gmail.com>
2023-11-26 16:13:07 -0300

    feat: creating page home in progress

--- frontend/src/app/Pages/Home.jsx
+++ frontend/src/app/Pages/Home.jsx
@@ -3,16 +3,41 @@ import IconBank from "../../assets/BANK-ICON.png";
 import IconInvestiment from "../../assets/INVESTIMENT.png";
 import IconRecipes from "../../assets/RECIPES.png";
 import IconExpenses from "../../assets/EXPENSE.png";
-import moment from "moment";
+import { useState } from "react";
+
+import Moment from "moment";
 
 function Home() {
-  const month = moment().format("M");
-  console.log(month);
+  Moment.locale("pt", {
+    months: [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
+    ],
+  });
+
+  const years = ["2023", "2022", "2021"];
+  const [currentMonth, setCurrentMonth] = useState("");
+  const date = Moment();
+
+  const months = date._locale._months;
+
+  const handleChangeDate = ({ target }) => {
+    setCurrentMonth(target.value);
+  };
 
   return (
     <div>
       <Header />
-
       <section>
         <div className="card-demonstrative balance">
           <span>
@@ -51,11 +76,88 @@ function Home() {
       </section>
 
       <section>
-        <div>
-          <button>{"<"}</button>
-          <span></span>
-          <button>{">"}</button>
-        </div>
+        <form>
+          <label className="month">
+            <select
+              name="date"
+              id="date"
+              value={currentMonth}
+              onChange={handleChangeDate}
+            >
+              <option value="" disabled>
+                Selecione um mês
+              </option>
+              {months &&
+                months.map((month) => (
+                  <option key={month} value={month}>
+                    {month}
+                  </option>
+                ))}{" "}
+              ;
+            </select>
+          </label>
+
+          <label className="year">
+            <select
+              name="date"
+              id="date"
+              value={currentMonth}
+              onChange={handleChangeDate}
+            >
+              <option value="" disabled>
+                Selecione um ano
+              </option>
+              {years &&
+                years.map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                ))}{" "}
+              ;
+            </select>
+          </label>
+        </form>
+
+        <table>
+          <thead>
+            <tr>
+              <td>Situação</td>
+              <td>Data</td>
+              <td>Descrição</td>
+              <td>Tipo</td>
+              <td>Valor</td>
+              <td>Ações</td>
+              <td>Entradas/Saída/Investimento</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>ICONE</td>
+              <td>10/12/2023</td>
+              <td>COMPRA NO CESTA</td>
+              <td>Despesa</td>
+              <td>R$ 542,56</td>
+              <td>
+                <button>Editar</button>
+                <button>Remover</button>
+              </td>
+              <td>STATUS</td>
+            </tr>
+
+            <tr>
+              <td>ICONE</td>
+              <td>09/12/2023</td>
+              <td>SALÁRIO DE NOVEMBRO</td>
+              <td>Despesa</td>
+              <td>R$ 542,56</td>
+              <td>
+                <button>Editar</button>
+                <button>Remover</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </section>
     </div>
   );
